0fe749253d312be2cbd0bdb1786630700a942c76
Sixstring982 <sixstring982@gmail.com>
2020-11-07 15:00:28 -0700

    Split Planent into two layers

--- src/model/cannon/planet.ts
+++ src/model/cannon/planet.ts
@@ -3,23 +3,34 @@ import { SCREEN_DIMENSIONS } from '../../util/screen';
 import { SceneConfig } from './scene_config';
 
 export class Planet {
-  private sprite: Sprite;
+  private background: Sprite;
+  private foreground: Sprite;
 
   create(scene: Phaser.Scene, sc: SceneConfig): Planet {
-    this.sprite = scene.physics.add.sprite(
+    this.background = scene.physics.add.sprite(
       sc.planetPivot.x,
       sc.planetPivot.y,
       'planet',
     );
 
-    this.sprite.scaleX = 16;
-    this.sprite.scaleY = 16;
+    this.background.scaleX = 16;
+    this.background.scaleY = 16;
+
+    this.foreground = scene.physics.add.sprite(
+      sc.planetPivot.x,
+      sc.planetPivot.y,
+      'planet',
+    );
+
+    this.foreground.scaleX = 16;
+    this.foreground.scaleY = 16;
 
     return this;
   }
 
   update(time: number, dt: number, sc: SceneConfig): Planet {
-    this.sprite.rotation = sc.rotation;
+    this.background.rotation = sc.rotation;
+    this.foreground.rotation = sc.rotation;
     return this;
   }
 }
